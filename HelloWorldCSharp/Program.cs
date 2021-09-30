c3377f425f312f4c6eaa91aa9ea6ec697cc58f47
frederik-schwarz <frederik@jens-schwarz.com>
2021-10-01 12:24:57 +1300

    console log var and data types

---
+++ HelloWorldCSharp/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorldCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 10;
+            int count = 9;
+            float currentFloat = 2.43f;
+            char character = 'h';
+            string firstName = "Fred";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(currentFloat);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
